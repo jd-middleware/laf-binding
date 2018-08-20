b6bd145d2efe7be55ec3d4718d3b2d17eaa24070
hexiaofeng <hexiaofeng@jd.com>
2018-08-20 17:31:06 +0800

    优化Map性能

--- src/main/java/com/jd/laf/binding/binder/Binders.java
+++ src/main/java/com/jd/laf/binding/binder/Binders.java
@@ -1,8 +1,8 @@
 package com.jd.laf.binding.binder;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ServiceLoader;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 /**
  * 绑定器管理器
@@ -10,7 +10,7 @@ import java.util.concurrent.ConcurrentMap;
 public abstract class Binders {
 
     //类对应的绑定器
-    protected static ConcurrentMap<Class<?>, Binder> plugins;
+    protected static volatile Map<Class<?>, Binder> plugins;
 
     /**
      * 获取绑定器
@@ -26,11 +26,11 @@ public abstract class Binders {
             //加载插件
             synchronized (Binders.class) {
                 if (plugins == null) {
-                    ConcurrentMap<Class<?>, Binder> result = new ConcurrentHashMap<Class<?>, Binder>();
+                    Map<Class<?>, Binder> result = new HashMap<Class<?>, Binder>();
                     //加载插件
                     ServiceLoader<Binder> loader = ServiceLoader.load(Binder.class, Binders.class.getClassLoader());
                     for (Binder binder : loader) {
-                        result.putIfAbsent(binder.annotation(), binder);
+                        result.put(binder.annotation(), binder);
                     }
                     plugins = result;
                 }
